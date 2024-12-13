199be186aa01485b2daa2f45bda32274939cd7e5
LiuSP <thodsaphonsp@gmail.com>
2024-12-13 12:19:20 +0700

    add page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './fetures/login/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path:"",
+  component: LoginComponent,
+  title:"Login"
+}];
--- src/app/fetures/login/login/login.component.ts
+++ src/app/fetures/login/login/login.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule, CheckBoxModule } from '@syncfusion/ej2-angular-buttons';
+import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [TextBoxModule, ButtonModule, CheckBoxModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
+  constructor() { }
 
+  public ngOnInit(): void {
+
+  }
+
+  public ngOnDestroy(): void {
+
+  }
 }
+
+
